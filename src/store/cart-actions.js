5b3ff5e1aafecead88b0d326da23236846538175
Alix Wawa <alixwawa@yahoo.com>
2023-05-21 15:51:31 -0500

    fixed .find bug by adding or logic

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -5,7 +5,7 @@ export const fetchCartData = () => {
 	return async (dispatch) => {
 		const fetchData = async () => {
 			const response = await fetch(
-				'https://react-http-77fd4-default-rtdb.firebaseio.com/cart.json'
+				'https://react-http-77fd4-default-rtdb.firebaseio.com/cart.json',
 			);
 			if (!response.ok) {
 				throw new Error('Could not fetch cart data!');
@@ -17,7 +17,12 @@ export const fetchCartData = () => {
 
 		try {
 			const cartData = await fetchData();
-			dispatch(cartActions.replaceCart(cartData));
+			dispatch(
+				cartActions.replaceCart({
+					items: cartData.items || [],
+					totalQuantity: cartData.totalQuantity,
+				}),
+			);
 		} catch (error) {
 			dispatch(
 				uiActions.showNotification({
@@ -45,7 +50,10 @@ export const sendCartData = (cart) => {
 				'https://react-http-77fd4-default-rtdb.firebaseio.com/cart.json',
 				{
 					method: 'PUT',
-					body: JSON.stringify(cart),
+					body: JSON.stringify({
+						items: cart.items,
+						totalQuantity: cart.totalQuantity,
+					}),
 				},
 			);
 
